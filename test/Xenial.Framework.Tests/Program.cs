212606a7a1a979dca394433a17e4b72a21db87b3
Manuel Grundner <m.grundner@delegate.at>
2020-09-04 11:57:13 +0200

    feat: build should now succeed

---
+++ test/Xenial.Framework.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using static Xenial.Tasty;
+
+namespace Xenial.Framework.Tests
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            It("Runs", () => true);
+
+            return await Run(args);
+        }
+    }
+}
---
+++ test/Xenial.Framework.Win.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using static Xenial.Tasty;
+
+namespace Xenial.Framework.Win.Tests
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            It("Runs", () => true);
+
+            return await Run(args);
+        }
+    }
+}
