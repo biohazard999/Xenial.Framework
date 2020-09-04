3dac1f16a59c3c730b85355bda4706e558bdae33
Manuel Grundner <m.grundner@delegate.at>
2020-09-04 10:18:47 +0200

    build: added build script

---
+++ build/Xenial.Build/Program.Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using static SimpleExec.Command;
+
+namespace Xenial.Build
+{
+    static partial class Program
+    {
+        static (string fullFramework, string netcore) FindTfms()
+        {
+            var dirProps = XElement.Load("Directory.Build.props");
+            var props = dirProps.Descendants("PropertyGroup");
+            var fullFramework = props.Descendants("FullFrameworkVersion").First().Value;
+            var netcore = props.Descendants("NetCoreVersion").First().Value;
+            return (fullFramework, netcore);
+        }
+
+        async static Task EnsureTools()
+        {
+            try
+            {
+                await RunAsync("dotnet", "format --version");
+            }
+            catch (SimpleExec.NonZeroExitCodeException)
+            {
+                //Can't find dotnet format, assuming tools are not installed
+                await RunAsync("dotnet", "tool restore");
+            }
+        }
+
+        static string Tabify(string s)
+            => string.Join(
+                Environment.NewLine, 
+                s.Split("\n").Select(s => $"\t{s}")
+            );
+    }
+}
---
+++ build/Xenial.Build/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using static SimpleExec.Command;
+using static Bullseye.Targets;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Xenial.Build
+{
+    internal static partial class Program
+    {
+        internal static async Task Main(string[] args)
+        {
+            static string logOptions(string target)
+                => $"/maxcpucount /nologo /verbosity:minimal /bl:./artifacts/logs/xenial.framework.{target}.binlog";
+
+            const string Configuration = "Release";
+
+            Func<string> properties = () => string.Join(" ", new Dictionary<string, string>
+            {
+                ["Configuration"] = Configuration,
+            }.Select(p => $"/P:{p.Key}=\"{p.Value}\""));
+
+            Target("ensure-tools", () => EnsureTools());
+
+            Target("clean", DependsOn("ensure-tools"),
+                () => RunAsync("dotnet", $"rimraf . -i **/bin/**/*.* -i **/obj/**/*.* -i artifacts/**/*.* -e node_modules/**/*.* -e build/**/*.* -q")
+            );
+
+            Target("lint", DependsOn("ensure-tools"),
+                () => RunAsync("dotnet", $"format --exclude ext --check --verbosity diagnostic")
+            );
+
+            Target("format", DependsOn("ensure-tools"),
+                () => RunAsync("dotnet", $"format --exclude ext")
+            );
+
+            Target("restore", DependsOn("lint"),
+                () => RunAsync("dotnet", $"restore {logOptions("restore")}")
+            );
+
+            Target("build", DependsOn("restore"),
+                () => RunAsync("dotnet", $"build --no-restore -c {Configuration} {logOptions("build")} {properties()}")
+            );
+
+            Target("test", DependsOn("build"), async () =>
+            {
+                var (fullFramework, netcore) = FindTfms();
+
+                var tfms = RuntimeInformation
+                            .IsOSPlatform(OSPlatform.Windows)
+                            ? new[] { fullFramework, netcore }
+                            : new[] { netcore };
+
+                var tests = tfms
+                    .Select(tfm => RunAsync("dotnet", $"run --project test/Xenial.Framework.Tests/Xenial.Framework.Tests.csproj --no-build --no-restore --framework {tfm} -c {Configuration} {properties()}"))
+                    .ToArray();
+
+                await Task.WhenAll(tests);
+            });
+
+            Target("lic", DependsOn("test"),
+                async () =>
+                {
+                    var files = Directory.EnumerateFiles(@"src", "*.csproj", SearchOption.AllDirectories).Select(file => new
+                    {
+                        ProjectName = $"src/{Path.GetFileNameWithoutExtension(file)}/{Path.GetFileName(file)}",
+                        ThirdPartyName = $"src/{Path.GetFileNameWithoutExtension(file)}/THIRD-PARTY-NOTICES.TXT"
+                    });
+
+                    var tasks = files.Select(proj => RunAsync("dotnet", $"thirdlicense --project {proj.ProjectName} --output {proj.ThirdPartyName}"));
+
+                    await Task.WhenAll(tasks);
+                }
+            );
+
+            Target("pack", DependsOn("lic"),
+                () => RunAsync("dotnet", $"pack Xenial.Framework.sln --no-restore --no-build -c {Configuration} {logOptions("pack.nuget")} {properties()}")
+            );
+
+            Target("docs",
+                () => RunAsync("dotnet", "wyam docs -o ../artifacts/docs")
+            );
+
+            Target("docs.serve", DependsOn("ensure-tools"),
+                () => RunAsync("dotnet", "wyam docs -o ../artifacts/docs -w -p")
+            );
+
+            Target("deploy.nuget", DependsOn("ensure-tools"), async () =>
+            {
+                var files = Directory.EnumerateFiles("artifacts/nuget", "*.nupkg");
+
+                foreach (var file in files)
+                {
+                    await RunAsync("dotnet", $"nuget push {file} --skip-duplicate -s https://api.nuget.org/v3/index.json -k {Environment.GetEnvironmentVariable("NUGET_AUTH_TOKEN")}",
+                        noEcho: true
+                    );
+                }
+            });
+
+            Target("release", async () =>
+            {
+                await Release();
+            });
+
+            Target("default", DependsOn("test"));
+
+            await RunTargetsAndExitAsync(args);
+        }
+    }
+}
