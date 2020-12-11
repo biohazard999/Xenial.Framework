2662400c29a364473650f13d65ff5855731838cc
Manuel Grundner <m.grundner@delegate.at>
2020-12-11 15:11:51 +0100

    feat: added ModuleTypeListExtentions

---
+++ src/Xenial.Framework/ModuleTypeListExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DevExpress.ExpressApp
+{
+    /// <summary>
+    /// Provides Extention Methods for <see cref="ModuleTypeList"/>
+    /// </summary>
+    public static class ModuleTypeListExtentions
+    {
+        /// <summary>
+        /// Adds types to the <see cref="ModuleTypeList"/>
+        /// </summary>
+        /// <param name="moduleTypeList">The module type list.</param>
+        /// <param name="types">The types.</param>
+        /// <returns>The original <see cref="ModuleTypeList"/></returns>
+        public static ModuleTypeList AndModuleTypes(this ModuleTypeList moduleTypeList, params Type[] types)
+        {
+            moduleTypeList.AddRange(types);
+            return moduleTypeList;
+        }
+    }
+}
