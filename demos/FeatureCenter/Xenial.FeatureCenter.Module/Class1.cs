8e06d3a952a451c466788cbe766e0a92ba6c87d7
Manuel Grundner <m.grundner@delegate.at>
2020-12-11 09:38:43 +0100

    feat: add democenter

---
+++ demos/FeatureCenter/Xenial.FeatureCenter.Module/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Xenial.FeatureCenter.Module
+{
+    public class Class1
+    {
+    }
+}
---
+++ demos/FeatureCenter/Xenial.FeatureCenter.Win/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+using DevExpress.ExpressApp.Win;
+
+namespace Xenial.FeatureCenter.Win
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            _ = true;
+        }
+    }
+
+    public class FeatureCenterWindowsFromsApplication : WinApplication
+    {
+
+    }
+}
