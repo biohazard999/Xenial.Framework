0853244351cdbc1b307ff19d4a3d0a0cacef3c44
Manuel Grundner <m.grundner@delegate.at>
2020-12-12 13:02:22 +0100

    feat: caption visible

--- demos/FeatureCenter/Xenial.FeatureCenter.Module/BusinessObjects/ModelBuilders/ModelBuilderBasicPropertiesDemo.cs
+++ demos/FeatureCenter/Xenial.FeatureCenter.Module/BusinessObjects/ModelBuilders/ModelBuilderBasicPropertiesDemo.cs
@@ -16,11 +16,25 @@ namespace Xenial.FeatureCenter.Module.BusinessObjects.ModelBuilders
     [ImageName("direction1")]
     public class ModelBuilderBasicPropertiesDemo : NonPersistentBaseObject
     {
+        private string? demoCode = "https://www.xenial.io";
+        [ImmediatePostData]
+        public string? DemoCode
+        {
+            get => demoCode; set
+            {
+                if (SetPropertyValue(ref demoCode, value))
+                {
+                    if (Uri.TryCreate(value, UriKind.RelativeOrAbsolute, out var newUri))
+                    {
+                        DemoUri = newUri;
+                    }
+                }
+            }
+        }
+
         private Uri? demoUri = new Uri("https://www.xenial.io");
         [EditorAlias("WebViewUriPropertyEditor")]
         public Uri? DemoUri { get => demoUri; set => SetPropertyValue(ref demoUri, value); }
 
-        private string? demoCode;
-        public string? DemoCode { get => demoCode; set => SetPropertyValue(ref demoCode, value); }
     }
 }
