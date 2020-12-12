05ce3bd7e1cfc50634470056b2ec03617b392875
Manuel Grundner <m.grundner@delegate.at>
2020-12-12 11:26:00 +0100

    feat: use singleton attribute

--- demos/FeatureCenter/Xenial.FeatureCenter.Module/BusinessObjects/ModelBuilders/ModelBuilderBasicPropertiesDemo.cs
+++ demos/FeatureCenter/Xenial.FeatureCenter.Module/BusinessObjects/ModelBuilders/ModelBuilderBasicPropertiesDemo.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 
 using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+
+using Xenial.Framework.Base;
 
 namespace Xenial.FeatureCenter.Module.BusinessObjects.ModelBuilders
 {
     [DomainComponent]
+    [Singleton]
+    [DefaultClassOptions]
     public class ModelBuilderBasicPropertiesDemo : DevExpress.ExpressApp.NonPersistentBaseObject
     {
         private string? demoCode;
--- demos/FeatureCenter/Xenial.FeatureCenter.Module/Module.cs
+++ demos/FeatureCenter/Xenial.FeatureCenter.Module/Module.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model.Core;
+
+using Xenial.FeatureCenter.Module.BusinessObjects.ModelBuilders;
 using Xenial.Framework;
 
 namespace Xenial.FeatureCenter.Module
 {
     public class FeatureCenterModule : XenialModuleBase
     {
+        protected override IEnumerable<Type> GetDeclaredExportedTypes()
+            => base.GetDeclaredExportedTypes()
+            .Concat(new[]
+            {
+                typeof(ModelBuilderBasicPropertiesDemo)
+            });
+
+        protected override IEnumerable<Type> GetDeclaredControllerTypes()
+            => base.GetDeclaredControllerTypes()
+                .UseSingletonController();
+
+        public override void AddGeneratorUpdaters(ModelNodesGeneratorUpdaters updaters)
+        {
+            updaters.UseSingletonNavigationItems();
+            base.AddGeneratorUpdaters(updaters);
+        }
     }
 }
