577544155ecf694a3fed2c5f4148056f0c2c936f
Manuel Grundner <m.grundner@delegate.at>
2020-12-18 08:49:25 +0100

    feat(blazor): use serilog

--- demos/FeatureCenter/Xenial.FeatureCenter.Blazor.Server/Program.cs
+++ demos/FeatureCenter/Xenial.FeatureCenter.Blazor.Server/Program.cs
@@ -14,6 +14,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
 namespace Xenial.FeatureCenter.Blazor.Server
 {
     internal static class Program
@@ -23,40 +27,69 @@ namespace Xenial.FeatureCenter.Blazor.Server
 
         public static int Main(string[] args)
         {
-            if (ContainsArgument(args, "help") || ContainsArgument(args, "h"))
+            Log.Logger = new LoggerConfiguration()
+              .MinimumLevel.Debug()
+              .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+              .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+              .MinimumLevel.Override("System", LogEventLevel.Warning)
+              .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+              .Enrich.FromLogContext()
+              .WriteTo.File(
+                  @"C:\logs\framework.featurecenter.xenial.io\framework.featurecenter.xenial.io.log",
+                  fileSizeLimitBytes: 1_000_000,
+                  rollOnFileSizeLimit: true,
+                  shared: true,
+                  flushToDiskInterval: TimeSpan.FromSeconds(1))
+              .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
+              .CreateLogger();
+
+            try
             {
-                Console.WriteLine("Updates the database when its version does not match the application's version.");
-                Console.WriteLine();
-                Console.WriteLine($"    {Assembly.GetExecutingAssembly().GetName().Name}.exe --updateDatabase [--forceUpdate --silent]");
-                Console.WriteLine();
-                Console.WriteLine("--forceUpdate - Marks that the database must be updated whether its version matches the application's version or not.");
-                Console.WriteLine("--silent - Marks that database update proceeds automatically and does not require any interaction with the user.");
-                Console.WriteLine();
-                Console.WriteLine($"Exit codes: 0 - {DBUpdater.StatusUpdateCompleted}");
-                Console.WriteLine($"            1 - {DBUpdater.StatusUpdateError}");
-                Console.WriteLine($"            2 - {DBUpdater.StatusUpdateNotNeeded}");
-            }
-            else
-            {
-                FrameworkSettings.DefaultSettingsCompatibilityMode = FrameworkSettingsCompatibilityMode.Latest;
-                var host = CreateHostBuilder(args).Build();
-                if (ContainsArgument(args, "updateDatabase"))
+                Log.Information("Starting host...");
+                if (ContainsArgument(args, "help") || ContainsArgument(args, "h"))
                 {
-                    using (var serviceScope = host.Services.CreateScope())
-                    {
-                        return serviceScope.ServiceProvider.GetRequiredService<IDBUpdater>().Update(ContainsArgument(args, "forceUpdate"), ContainsArgument(args, "silent"));
-                    }
+                    Console.WriteLine("Updates the database when its version does not match the application's version.");
+                    Console.WriteLine();
+                    Console.WriteLine($"    {Assembly.GetExecutingAssembly().GetName().Name}.exe --updateDatabase [--forceUpdate --silent]");
+                    Console.WriteLine();
+                    Console.WriteLine("--forceUpdate - Marks that the database must be updated whether its version matches the application's version or not.");
+                    Console.WriteLine("--silent - Marks that database update proceeds automatically and does not require any interaction with the user.");
+                    Console.WriteLine();
+                    Console.WriteLine($"Exit codes: 0 - {DBUpdater.StatusUpdateCompleted}");
+                    Console.WriteLine($"            1 - {DBUpdater.StatusUpdateError}");
+                    Console.WriteLine($"            2 - {DBUpdater.StatusUpdateNotNeeded}");
                 }
                 else
                 {
-                    host.Run();
+                    FrameworkSettings.DefaultSettingsCompatibilityMode = FrameworkSettingsCompatibilityMode.Latest;
+                    var host = CreateHostBuilder(args).Build();
+                    if (ContainsArgument(args, "updateDatabase"))
+                    {
+                        using var serviceScope = host.Services.CreateScope();
+                        return serviceScope.ServiceProvider.GetRequiredService<IDBUpdater>().Update(ContainsArgument(args, "forceUpdate"), ContainsArgument(args, "silent"));
+                    }
+                    else
+                    {
+                        Log.Information("Host run...");
+                        host.Run();
+                    }
                 }
+                return 0;
             }
-            return 0;
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
