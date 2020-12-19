7c0b121664721ab2827d8fdbe1c1b7474aedc147
Manuel Grundner <m.grundner@delegate.at>
2020-12-19 10:49:48 +0100

    test(modelbuilders): missing assert extensions

---
+++ test/Xenial.Framework.Tests/ModelBuilders/Assertions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+using DevExpress.ExpressApp.Model;
+
+using Shouldly;
+
+using Xenial.Framework.ModelBuilders;
+
+namespace Xenial.Framework.Tests.ModelBuilders
+{
+    internal static class Assertions
+    {
+        public static IModelBuilder<T> AssertModelDefaultAttribute<T>(this IModelBuilder<T> builder, string propertyName, string propertyValue)
+        {
+            var attr = builder.TypeInfo.FindAttributes<ModelDefaultAttribute>().FirstOrDefault(a => a.PropertyName == propertyName);
+
+            attr.ShouldSatisfyAllConditions
+            (
+                () => attr.ShouldNotBeNull(),
+                () => attr!.PropertyName.ShouldBe(propertyName),
+                () => attr!.PropertyValue.ShouldBe(propertyValue)
+            );
+            return builder;
+        }
+
+        public static IModelBuilder<TClassType> AssertAttribute<TAttribute, TClassType>(this IModelBuilder<TClassType> builder, Func<TAttribute, bool> assertion)
+               where TAttribute : Attribute
+        {
+            var attr = builder.TypeInfo.FindAttribute<TAttribute>();
+
+            attr.ShouldSatisfyAllConditions
+            (
+                () => attr.ShouldNotBeNull(),
+                () => assertion.Invoke(attr).ShouldBe(true)
+            );
+            return builder;
+        }
+
+    }
+}
