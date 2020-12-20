51abfdc044ed927b576280181b4808a71d762021
Manuel Grundner <m.grundner@delegate.at>
2020-12-20 10:15:21 +0100

    feat(modelbuilders): added editor extentions

---
+++ src/Xenial.Framework/ModelBuilders/PropertyBuilderExtensions.EditorAliases.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using DevExpress.ExpressApp.Editors;
+
+namespace Xenial.Framework.ModelBuilders
+{
+    public static partial class PropertyBuilderExtensions
+    {
+        /// <summary>
+        /// Uses the built-in alias for the BooleanPropertyEditor and ASPxBooleanPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<bool?, TClassType> UsingBooleanPropertyEditor<TClassType>(this IPropertyBuilder<bool?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.BooleanPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the BooleanPropertyEditor and ASPxBooleanPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<bool, TClassType> UsingBooleanPropertyEditor<TClassType>(this IPropertyBuilder<bool, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.BooleanPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the StringPropertyEditor and ASPxStringPropertyEditor 
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<string?, TClassType> UsingStringPropertyEditor<TClassType>(this IPropertyBuilder<string?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.StringPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the RichTextPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<string?, TClassType> UsingRichTextPropertyEditor<TClassType>(this IPropertyBuilder<string?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.RichTextPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the RichTextPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<byte[]?, TClassType> UsingRichTextPropertyEditor<TClassType>(this IPropertyBuilder<byte[]?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.RichTextPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the BytePropertyEditor and ASPxBytePropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<byte, TClassType> UsingBytePropertyEditor<TClassType>(this IPropertyBuilder<byte, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.BytePropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the BytePropertyEditor and ASPxBytePropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<byte?, TClassType> UsingBytePropertyEditor<TClassType>(this IPropertyBuilder<byte?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.BytePropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the DecimalPropertyEditor and ASPxDecimalPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<decimal, TClassType> UsingDecimalPropertyEditor<TClassType>(this IPropertyBuilder<decimal, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.DecimalPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the DecimalPropertyEditor and ASPxDecimalPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<decimal?, TClassType> UsingDecimalPropertyEditor<TClassType>(this IPropertyBuilder<decimal?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.DecimalPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the DoublePropertyEditor and ASPxDoublePropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<double, TClassType> UsingDoublePropertyEditor<TClassType>(this IPropertyBuilder<double, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.DoublePropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the DoublePropertyEditor and ASPxDoublePropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<double?, TClassType> UsingDoublePropertyEditor<TClassType>(this IPropertyBuilder<double?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.DoublePropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the FloatPropertyEditor and ASPxFloatPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<float, TClassType> UsingFloatPropertyEditor<TClassType>(this IPropertyBuilder<float, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.FloatPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the FloatPropertyEditor and ASPxFloatPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<float?, TClassType> UsingFloatPropertyEditor<TClassType>(this IPropertyBuilder<float?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.FloatPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<short, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<short, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<short?, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<short?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<ushort, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<ushort, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<ushort?, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<ushort?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<int, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<int, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<int?, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<int?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<uint, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<uint, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<uint?, TClassType> UsingIntegerPropertyEditor<TClassType>(this IPropertyBuilder<uint?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<long, TClassType> UsingLongPropertyEditor<TClassType>(this IPropertyBuilder<long, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the LongPropertyEditor and ASPxLongPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<long?, TClassType> UsingLongPropertyEditor<TClassType>(this IPropertyBuilder<long?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the IntegerPropertyEditor and ASPxIntPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<ulong, TClassType> UsingLongPropertyEditor<TClassType>(this IPropertyBuilder<ulong, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+
+        /// <summary>
+        /// Uses the built-in alias for the LongPropertyEditor and ASPxLongPropertyEditor
+        /// </summary>
+        /// <typeparam name="TClassType">The type of the type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IPropertyBuilder<ulong?, TClassType> UsingLongPropertyEditor<TClassType>(this IPropertyBuilder<ulong?, TClassType> builder)
+            => builder.UsingEditorAlias(EditorAliases.IntegerPropertyEditor);
+    }
+}
--- test/Xenial.Framework.Tests/ModelBuilders/PropertyBuilderExtentionsFacts.cs
+++ test/Xenial.Framework.Tests/ModelBuilders/PropertyBuilderExtentionsFacts.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 using Bogus;
 
 using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
 
 using Shouldly;
 
@@ -91,7 +93,127 @@ namespace Xenial.Framework.Tests.ModelBuilders
                         .HasCaption(attributeValue)
                         .AssertModelDefaultAttribute("Caption", attributeValue);
                 });
+
+                It("IsPassword", () =>
+                {
+                    var (builder, _) = CreateBuilder();
+
+                    builder.For(p => p.StringProperty)
+                        .IsPassword()
+                        .AssertModelDefaultAttribute("IsPassword", true.ToString());
+                });
+
+                Describe("PredefinedValues", () =>
+                {
+                    It("with string values", () =>
+                    {
+                        var (builder, _) = CreateBuilder();
+
+                        var expectedValue = "foo;bar";
+
+                        builder.For(p => p.StringProperty)
+                            .WithPredefinedValues(expectedValue)
+                            .AssertModelDefaultAttribute("PredefinedValues", expectedValue);
+                    });
+
+                    It("with array values", () =>
+                    {
+                        var (builder, _) = CreateBuilder();
+
+                        var actualValue = new[] { "foo", "bar", "baz" };
+                        var expectedValue = "foo;bar;baz";
+
+                        builder.For(p => p.StringProperty)
+                            .WithPredefinedValues(expectedValue)
+                            .AssertModelDefaultAttribute("PredefinedValues", expectedValue);
+                    });
+                });
+
+                Describe("ToolTips", () =>
+                {
+                    It("has ToolTip", () =>
+                    {
+                        var (builder, faker) = CreateBuilder();
+
+                        var expectedValue = faker.Random.String();
+
+                        builder.For(p => p.StringProperty)
+                            .HasTooltip(expectedValue)
+                            .AssertModelDefaultAttribute("ToolTip", expectedValue);
+                    });
+
+                    It("has ToolTip title", () =>
+                    {
+                        var (builder, faker) = CreateBuilder();
+
+                        var expectedValue = faker.Random.String();
+
+                        builder.For(p => p.StringProperty)
+                            .HasTooltipTitle(expectedValue)
+                            .AssertModelDefaultAttribute("ToolTipTitle", expectedValue);
+                    });
+
+                    It("has ToolTip title", () =>
+                    {
+                        var (builder, faker) = CreateBuilder();
+
+                        var expectedValue = faker.Random.Enum<ToolTipIconType>();
+
+                        builder.For(p => p.StringProperty)
+                            .HasTooltipIconType(expectedValue)
+                            .AssertModelDefaultAttribute("ToolTipIconType", expectedValue.ToString());
+                    });
+                });
+
+                It("DisplayFormat", () =>
+                {
+                    var (builder, _) = CreateBuilder();
+
+                    var expectedValue = "{0:MyFormat}";
+
+                    builder.For(p => p.StringProperty)
+                        .HasDisplayFormat(expectedValue)
+                        .AssertModelDefaultAttribute("DisplayFormat", expectedValue);
+                });
+
+                It("PropertyEditor", () =>
+                {
+                    It("using type overload", () =>
+                    {
+                        var (builder, _) = CreateBuilder();
+
+                        builder.For(p => p.StringProperty)
+                            .UsingPropertyEditor(typeof(FakePropertyEditor))
+                            .AssertModelDefaultAttribute("PropertyEditorType", typeof(FakePropertyEditor).FullName!);
+                    });
+
+                    It("using string overload", () =>
+                    {
+                        var (builder, _) = CreateBuilder();
+
+                        builder.For(p => p.StringProperty)
+                            .UsingPropertyEditor(typeof(FakePropertyEditor).FullName!)
+                            .AssertModelDefaultAttribute("PropertyEditorType", typeof(FakePropertyEditor).FullName!);
+                    });
+
+                    It("using generic overload", () =>
+                    {
+                        var (builder, _) = CreateBuilder();
+
+                        builder.For(p => p.StringProperty)
+                            .UsingPropertyEditor(o => o.Editor<FakePropertyEditor>())
+                            .AssertModelDefaultAttribute("PropertyEditorType", typeof(FakePropertyEditor).FullName!);
+                    });
+                });
             });
         });
+    }
+
+    public class FakePropertyEditor : PropertyEditor
+    {
+        public FakePropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
+        protected override object CreateControlCore() => throw new NotImplementedException();
+        protected override object GetControlValueCore() => throw new NotImplementedException();
+        protected override void ReadValueCore() => throw new NotImplementedException();
     }
 }
