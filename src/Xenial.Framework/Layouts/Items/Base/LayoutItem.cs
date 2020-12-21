fe0cd67e9a025cb6180d9833e309e7bf4d05b96e
Manuel Grundner <m.grundner@delegate.at>
2020-12-21 21:15:07 +0100

    feat: added base layout item records

---
+++ src/Xenial.Framework/Layouts/Items/Base/LayoutItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xenial.Framework.Layouts.Items.Base
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public record LayoutItem : LayoutItemBase
+    {
+    }
+}
