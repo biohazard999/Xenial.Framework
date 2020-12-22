dd9827cbcba3ca16f4ef17579558f7208f99fe62
Manuel Grundner <m.grundner@delegate.at>
2020-12-22 18:20:11 +0100

    fix(lic): switched target and props

--- src/Xenial.Framework/Layouts/Items/Base/Layout.cs
+++ src/Xenial.Framework/Layouts/Items/Base/Layout.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+#pragma warning disable CA1724 //Conflicts with Winforms: Should not conflict in practice
+
 namespace Xenial.Framework.Layouts.Items.Base
 {
     /// <summary>
