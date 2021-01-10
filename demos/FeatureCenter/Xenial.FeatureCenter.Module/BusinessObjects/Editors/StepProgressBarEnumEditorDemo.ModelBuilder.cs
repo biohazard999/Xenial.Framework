0ce3a572ae53185a42af9b2314e8509919dfddd4
Manuel Grundner <m.grundner@delegate.at>
2021-01-10 11:08:04 +0100

    feat(layouts): overloads for layout groups

---
+++ demos/FeatureCenter/Xenial.FeatureCenter.Module/BusinessObjects/Editors/StepProgressBarEnumEditorDemo.ModelBuilder.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.ExpressApp.DC;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xenial.Framework.ModelBuilders;
+
+namespace Xenial.FeatureCenter.Module.BusinessObjects.Editors
+{
+    public class StepProgressBarEnumEditorDemoModelBuilder : ModelBuilder<StepProgressBarEnumEditorPersistentDemo>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StepProgressBarEnumEditorDemoModelBuilder`1"/> class.
+        /// </summary>
+        /// <param name="typeInfo">The type information.</param>
+        public StepProgressBarEnumEditorDemoModelBuilder(ITypeInfo typeInfo) : base(typeInfo) { }
+
+        public override void Build()
+        {
+            base.Build();
+
+        }
+    }
+}
