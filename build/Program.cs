f7728549ccf69dcf58226594cf1a9bc9040dda58
Manuel Grundner <m.grundner@delegate.at>
2021-01-17 20:49:27 +0100

    build: try to get package restore correct

--- build/Program.cs
+++ build/Program.cs
@@ -149,9 +149,9 @@ namespace Xenial.Build
 
             Target("publish:Xenial.FeatureCenter.Win", DependsOn("pack"), async () =>
             {
-                await RunAsync("dotnet", "zip install", workingDirectory: "demos/FeatureCenter/Xenial.FeatureCenter.Win");
+                await RunAsync("dotnet", "zip install", workingDirectory: "demos/FeatureCenter");
 
-                await RunAsync("dotnet", $"msbuild demos/FeatureCenter/Xenial.FeatureCenter.Win/Xenial.FeatureCenter.Win.csproj /t:Restore;Build;Publish;CreateZip {logOptions("publish:Xenial.FeatureCenter.Win")} {GetProperties()} /p:PackageVersion={version} /p:PackageName=Xenial.FeatureCenter.Win.v{version}.AnyCpu /p:PackageDir={artifactsDirectors}");
+                await RunAsync("dotnet", $"msbuild demos/FeatureCenter/Xenial.FeatureCenter.Win/Xenial.FeatureCenter.Win.csproj /t:Restore;Build;Publish;CreateZip {logOptions("publish:Xenial.FeatureCenter.Win")} {GetProperties()} /p:PackageVersion={version} /p:XenialDemoPackageVersion={version} /p:XenialDebug=false /p:PackageName=Xenial.FeatureCenter.Win.v{version}.AnyCpu /p:PackageDir={artifactsDirectors}");
             });
 
             BuildAndDeployIISProject(new IISDeployOptions("Xenial.FeatureCenter.Blazor.Server", "framework.featurecenter.xenial.io")
