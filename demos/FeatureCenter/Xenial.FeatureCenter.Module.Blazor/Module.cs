ac89c21a6442c55250cff32014b18e4350040b5d
Manuel Grundner <m.grundner@delegate.at>
2021-01-18 15:46:32 +0100

    feat(webview): added blazor webview module

--- demos/FeatureCenter/Xenial.FeatureCenter.Module.Blazor/Module.cs
+++ demos/FeatureCenter/Xenial.FeatureCenter.Module.Blazor/Module.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 
 using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.Editors;
-using DevExpress.Xpo;
 
-using Xenial.FeatureCenter.Module.BusinessObjects.Editors;
 using Xenial.Framework;
 using Xenial.Framework.TokenEditors.Blazor;
+using Xenial.Framework.WebView.Blazor;
 
 namespace Xenial.FeatureCenter.Module.Blazor
 {
@@ -18,7 +16,8 @@ namespace Xenial.FeatureCenter.Module.Blazor
             => base.GetRequiredModuleTypesCore()
                 .AndModuleTypes(
                     typeof(FeatureCenterModule),
-                    typeof(XenialTokenEditorsBlazorModule)
+                    typeof(XenialTokenEditorsBlazorModule),
+                    typeof(XenialWebViewBlazorModule)
                 );
     }
 }
