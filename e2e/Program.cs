33207e24dd7c5feb86b532ee696ec52c193fe673
Manuel Grundner <m.grundner@delegate.at>
2021-01-22 12:35:46 +0100

    feat(e2e): added basic e2e project

---
+++ e2e/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace e2e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
