a41a35432cdedfbd03dee7237f264fb8e1398742
Manuel Grundner <m.grundner@delegate.at>
2021-01-25 09:22:25 +0100

    refactor(demos): TextSizeIndexable constant

--- demos/MailClient/MailClient.Module/BusinessObjects/Mail.cs
+++ demos/MailClient/MailClient.Module/BusinessObjects/Mail.cs
@@ -10,57 +10,52 @@ namespace MailClient.Module.BusinessObjects
     [Persistent("Mails")]
     public class Mail : MailBaseObjectId
     {
+        // 50000 chosen to be explicit to allow enough size to avoid truncation, yet stay beneath the MySql row size limit of ~65K
+        // apparently anything over 4K converts to nvarchar(max) on SqlServer
+        private const int textSizeIndexable = 50000;
+
         public Mail(Session session) : base(session) { }
 
         [RuleRequiredField(DefaultContexts.Save)]
-        public MailAccount Account { get; set; }
-        public MailDirection Direction { get; set; }
+        public MailAccount? Account { get; set; }
+
+        public MailDirection Direction { get; set; } = MailDirection.Inbound;
 
         [Persistent("From")]
-        // 50000 chosen to be explicit to allow enough size to avoid truncation, yet stay beneath the MySql row size limit of ~65K
-        // apparently anything over 4K converts to nvarchar(max) on SqlServer
-        [Size(50000)]
+        [Size(textSizeIndexable)]
         [Indexed]
-        public string From { get; set; }
+        public string? From { get; set; }
 
         [Persistent("To")]
-        // 50000 chosen to be explicit to allow enough size to avoid truncation, yet stay beneath the MySql row size limit of ~65K
-        // apparently anything over 4K converts to nvarchar(max) on SqlServer
-        [Size(50000)]
+        [Size(textSizeIndexable)]
         [Indexed]
-        public string To { get; set; }
+        public string? To { get; set; }
 
         [Persistent("CC")]
-        // 50000 chosen to be explicit to allow enough size to avoid truncation, yet stay beneath the MySql row size limit of ~65K
-        // apparently anything over 4K converts to nvarchar(max) on SqlServer
-        [Size(50000)]
+        [Size(textSizeIndexable)]
         [Indexed]
-        public string CC { get; set; }
+        public string? CC { get; set; }
 
         [Persistent("BCC")]
-        // 50000 chosen to be explicit to allow enough size to avoid truncation, yet stay beneath the MySql row size limit of ~65K
-        // apparently anything over 4K converts to nvarchar(max) on SqlServer
-        [Size(50000)]
+        [Size(textSizeIndexable)]
         [Indexed]
-        public string BCC { get; set; }
+        public string? BCC { get; set; }
 
         [Persistent("Subject")]
-        // 50000 chosen to be explicit to allow enough size to avoid truncation, yet stay beneath the MySql row size limit of ~65K
-        // apparently anything over 4K converts to nvarchar(max) on SqlServer
-        [Size(50000)]
+        [Size(textSizeIndexable)]
         [Indexed]
-        public string Subject { get; set; }
+        public string? Subject { get; set; }
 
         [Persistent("Sent")]
         public DateTime? Sent { get; set; }
 
         [Persistent("TextBody")]
         [Size(SizeAttribute.Unlimited)]
-        public string TextBody { get; set; }
+        public string? TextBody { get; set; }
 
         [Persistent("HtmlBody")]
         [Size(SizeAttribute.Unlimited)]
-        public string HtmlBody { get; set; }
+        public string? HtmlBody { get; set; }
     }
 
     public enum MailDirection
