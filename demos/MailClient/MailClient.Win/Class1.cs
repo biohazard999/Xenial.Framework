e5fdbb298db293a1beae87055aa582c90c73d074
Manuel Grundner <m.grundner@delegate.at>
2021-01-25 08:06:23 +0100

    feat(demos): winforms mail client

--- demos/MailClient/MailClient.Win/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MailClient.Win
-{
-    public class Class1
-    {
-    }
-}
---
+++ demos/MailClient/MailClient.Win/MailClientWindowsFormsApplication.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.XtraPrinting;
+
+namespace MailClient.Win
+{
+    public class MailClientWindowsFormsApplication : WinApplication
+    {
+        static MailClientWindowsFormsApplication()
+            => DevExpress.Xpo.DB.MySqlConnectionProvider.Register();
+
+        public MailClientWindowsFormsApplication()
+        {
+            IgnoreUserModelDiffs = true;
+            ConnectionString = DevExpress.Xpo.DB.MySqlConnectionProvider.GetConnectionString("localhost", "root", "root", "MailClient");
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            CheckCompatibilityType = CheckCompatibilityType.DatabaseSchema;
+        }
+
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args)
+        {
+            _ = args ?? throw new ArgumentNullException(nameof(args));
+            args.ObjectSpaceProviders.Add(new XPObjectSpaceProvider(
+                XPObjectSpaceProvider.GetDataStoreProvider(args.ConnectionString, args.Connection, true),
+                true
+            ));
+            args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
+        }
+
+        protected override void OnDatabaseVersionMismatch(DatabaseVersionMismatchEventArgs args)
+        {
+            _ = args ?? throw new ArgumentNullException(nameof(args));
+            args.Updater.Update();
+            args.Handled = true;
+            base.OnDatabaseVersionMismatch(args);
+        }
+    }
+}
