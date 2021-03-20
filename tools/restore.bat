1a03a374bf990d8c733adb2640f717fef0c080de
Manuel Grundner <m.grundner@delegate.at>
2021-03-20 09:38:57 +0100

    chore: warnings not as errors

--- tools/restore.bat
+++
@@ -1,2 +0,0 @@
-if not exist nuget.exe powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe"
-nuget.exe install -ExcludeVersion -OutputDirectory packages -ConfigFile nuget.config
