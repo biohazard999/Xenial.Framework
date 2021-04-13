86a5931b6849a929f3e34489b7e7d852fa41bc88
Manuel Grundner <m.grundner@delegate.at>
2021-04-13 10:38:33 +0200

    build: automatically refresh env

--- b.bat
+++ b.bat
@@ -1,15 +1,17 @@
 @echo off
 @pushd %~dp0
+SET DEVEXPRESS_NUGET_FEED_DEFAULT=artifacts\nuget
 if defined DEVEXPRESS_NUGET_FEED (
-    echo "%%DEVEXPRESS_NUGET_FEED%% is set"
-    if "%DEVEXPRESS_NUGET_FEED%"=="artifacts\nuget" (
-        echo "%%DEVEXPRESS_NUGET_FEED%% is local source you only will be able to build docs"    
+    echo %%DEVEXPRESS_NUGET_FEED%% is set
+    if "%DEVEXPRESS_NUGET_FEED%"=="%DEVEXPRESS_NUGET_FEED_DEFAULT%" (
+        echo %%DEVEXPRESS_NUGET_FEED%% is local source you only will be able to build docs
     )
 ) ELSE (
-    echo "%%DEVEXPRESS_NUGET_FEED%% is not set, setting it to ./artifacts/nuget"
-    setx /M DEVEXPRESS_NUGET_FEED "artifacts\nuget"
-    echo "PLEASE RESTART YOUR CONSOLE"
-    EXIT
+    echo %%DEVEXPRESS_NUGET_FEED%% is not set, setting it to %DEVEXPRESS_NUGET_FEED_DEFAULT%
+    echo %%DEVEXPRESS_NUGET_FEED%% is local source you only will be able to build docs
+    setx /M DEVEXPRESS_NUGET_FEED %DEVEXPRESS_NUGET_FEED_DEFAULT%
+    echo Refreshing EnvironmentVariables
+    call refreshenv.bat
 )
 
 @dotnet run --project ".\build\build.csproj" -- %*
