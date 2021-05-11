0aad0fac0a2d932e2e72a92d8c31bf840286d099
Manuel Grundner <m.grundner@delegate.at>
2021-05-11 11:58:55 +0200

    test: added enabled and disabled tests

--- test/Xenial.Framework.Win.Tests/Program.cs
+++ test/Xenial.Framework.Win.Tests/Program.cs
@@ -64,6 +64,58 @@ namespace Xenial.Framework.Win.Tests
                     controller.OpenViewInModelEditorSimpleAction.Active.ResultValue.ShouldBe(false);
                     editModelController.Active.RemoveItem(key);
                 });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenBOModelInModelEditorSimpleAction)} should be active by default", () =>
+                {
+                    controller.OpenBOModelInModelEditorSimpleAction.Active.ResultValue.ShouldBe(true);
+                });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenBOModelInModelEditorSimpleAction)} should be deactivated", () =>
+                {
+                    const string key = "Test";
+                    editModelController.Active[key] = false;
+                    controller.OpenBOModelInModelEditorSimpleAction.Active.ResultValue.ShouldBe(false);
+                    editModelController.Active.RemoveItem(key);
+                });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenViewInModelEditorSimpleAction)} should be active by default", () =>
+                {
+                    controller.OpenViewInModelEditorSimpleAction.Active.ResultValue.ShouldBe(true);
+                });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenViewInModelEditorSimpleAction)} should be deactivated", () =>
+                {
+                    const string key = "Test";
+                    editModelController.Active[key] = false;
+                    controller.OpenViewInModelEditorSimpleAction.Active.ResultValue.ShouldBe(false);
+                    editModelController.Active.RemoveItem(key);
+                });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenBOModelInModelEditorSimpleAction)} should be enabled by default", () =>
+                {
+                    controller.OpenBOModelInModelEditorSimpleAction.Enabled.ResultValue.ShouldBe(true);
+                });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenBOModelInModelEditorSimpleAction)} should be disabled", () =>
+                {
+                    const string key = "Test";
+                    editModelController.EditModelAction.Enabled[key] = false;
+                    controller.OpenBOModelInModelEditorSimpleAction.Enabled.ResultValue.ShouldBe(false);
+                    editModelController.EditModelAction.Enabled.RemoveItem(key);
+                });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenViewInModelEditorSimpleAction)} should be enabled by default", () =>
+                {
+                    controller.OpenViewInModelEditorSimpleAction.Enabled.ResultValue.ShouldBe(true);
+                });
+
+                It($"{nameof(XenialAdvancedModelEditorActionsViewController.OpenViewInModelEditorSimpleAction)} should be disabled", () =>
+                {
+                    const string key = "Test";
+                    editModelController.EditModelAction.Enabled[key] = false;
+                    controller.OpenViewInModelEditorSimpleAction.Enabled.ResultValue.ShouldBe(false);
+                    editModelController.EditModelAction.Enabled.RemoveItem(key);
+                });
             });
 
             return await Run(args);
