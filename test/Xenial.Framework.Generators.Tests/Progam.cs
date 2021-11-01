bb69a859e75e1027edc8ae86efcd539445a987a2
Manuel Grundner <m.grundner@delegate.at>
2021-11-01 15:15:30 +0100

    feat: add net6 and file scoped program.cs

---
+++ test/Xenial.Framework.Generators.Tests/Progam.cs
@@ -0,0 +1,7 @@
+﻿using static Xenial.Tasty;
+
+Describe("Xenial.Framework.Generators", () =>
+{
+});
+
+return await Run(args);
---
+++ test/Xenial.Framework.Generators.Tests/_DuckTyping.cs
@@ -0,0 +1,10 @@
+﻿#if !NET5
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit { }
+}
+#endif
