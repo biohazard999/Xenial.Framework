761c7b4823f9a1ea81df6eb98b10667d6e360fa1
Manuel Grundner <m.grundner@delegate.at>
2021-11-28 17:10:20 +0100

    chore: debug

--- src/Xenial.Framework.Generators/XenialActionGenerator.cs
+++ src/Xenial.Framework.Generators/XenialActionGenerator.cs
@@ -234,6 +234,8 @@ public class XenialActionGenerator : ISourceGenerator
                             var method = methods.FirstOrDefault(method => method.Identifier.Text == "Execute");
                             if (method is not null)
                             {
+
+
                                 var returnTypeSymbol = semanticModel.GetDeclaredSymbol(method.ReturnType, context.CancellationToken);
 
                                 if (returnTypeSymbol is not null)
