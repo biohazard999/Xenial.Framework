56c139ae4673ae344bca7d093e4d83ef4003e58e
Manuel Grundner <m.grundner@delegate.at>
2021-11-29 23:07:59 +0100

    feat: image names in demo

---
+++ demos/SourceGenerators/Acme.Module/ImageNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xenial;
+
+namespace Acme.Module
+{
+    [XenialImageNames(
+        SmartComments = true,
+        ResourceAccessors = true
+    )]
+    internal partial class ImageNames
+    {
+    }
+}
