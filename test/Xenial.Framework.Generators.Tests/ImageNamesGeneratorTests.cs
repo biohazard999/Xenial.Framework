727344cc99d80a508ec08c889a2bd43f0b898009
Manuel Grundner <m.grundner@delegate.at>
2021-12-01 15:24:27 +0100

    refactor: image name tests cleanup

--- test/Xenial.Framework.Generators.Tests/ImageNamesGeneratorTests.cs
+++ test/Xenial.Framework.Generators.Tests/ImageNamesGeneratorTests.cs
@@ -305,48 +305,12 @@ public partial class MyGlobalClass
     }
 }
 
-internal static class CompilationHelpers
+internal static partial class CompilationHelpers
 {
     public static CSharpCompilation AddInlineXenialImageNamesAttribute(this CSharpCompilation compilation, string visibility = "internal")
     {
         (_, var syntaxTree) = XenialImageNamesGenerator.GenerateXenialImageNamesAttribute(visibility: visibility);
 
         return compilation.AddSyntaxTrees(syntaxTree);
-    }
-
-    public static (
-        GeneratorDriver driver,
-        ImmutableArray<Diagnostic> diagnostics,
-        Exception? emitException,
-        Type? generatedType
-    ) CompileAndLoadType(
-        this GeneratorDriver driver,
-        Compilation compilation,
-        string typeToLoad
-    )
-    {
-        driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out var compilation2, out var diag);
-
-        using var stream = new MemoryStream();
-        var emitResults = compilation2.Emit(stream);
-        if (emitResults is not null)
-        {
-            stream.Position = 0;
-#pragma warning disable CA1031 // Do not catch general exception types
-            try
-            {
-                var assembly = Assembly.Load(stream.ToArray());
-
-                return (driver, emitResults.Diagnostics, null, assembly.GetType(typeToLoad));
-            }
-            catch (Exception ex)
-            {
-                return (driver, emitResults.Diagnostics, ex, null);
-            }
-#pragma warning restore CA1031 // Do not catch general exception types
-        }
-
-        return (driver, ImmutableArray<Diagnostic>.Empty, null, null);
-
     }
 }
