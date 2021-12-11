009ea4e9548285ece8221394d3443cdc57faa595
Manuel Grundner <m.grundner@delegate.at>
2021-12-11 11:35:10 +0100

    fix: moved hot reload window

--- src/Xenial.Framework.Win/SystemModule/Controllers/XenialHotReloadDetailViewController.cs
+++ src/Xenial.Framework.Win/SystemModule/Controllers/XenialHotReloadDetailViewController.cs
@@ -19,54 +19,6 @@ namespace Xenial.Framework.Win.SystemModule;
 /// <summary>
 /// For internal use only.
 /// </summary>
-public sealed class XenialHotReloadMainWindowController : WindowController
-{
-    /// <summary>
-    /// For internal use only.
-    /// </summary>
-    public XenialHotReloadMainWindowController()
-        => TargetWindowType = WindowType.Main;
-
-    /// <summary>
-    /// For internal use only.
-    /// </summary>
-    protected override void OnActivated()
-    {
-        base.OnActivated();
-        var controller = Frame.GetController<WindowTemplateController>();
-        if (controller is not null)
-        {
-            controller.CustomizeWindowStatusMessages -= Controller_CustomizeWindowStatusMessages;
-            controller.CustomizeWindowStatusMessages += Controller_CustomizeWindowStatusMessages;
-        }
-    }
-
-    private void Controller_CustomizeWindowStatusMessages(object? sender, CustomizeWindowStatusMessagesEventArgs e)
-    {
-        var hotreloadPipeName = System.Environment.GetEnvironmentVariable("DOTNET_HOTRELOAD_NAMEDPIPE_NAME");
-        if (!string.IsNullOrEmpty(hotreloadPipeName))
-        {
-            e.StatusMessages.Add("Hot-Reload enabled");
-        }
-    }
-
-    /// <summary>
-    /// For internal use only.
-    /// </summary>
-    protected override void OnDeactivated()
-    {
-        var controller = Frame.GetController<WindowTemplateController>();
-        if (controller is not null)
-        {
-            controller.CustomizeWindowStatusMessages -= Controller_CustomizeWindowStatusMessages;
-        }
-        base.OnDeactivated();
-    }
-}
-
-/// <summary>
-/// For internal use only.
-/// </summary>
 public sealed class XenialHotReloadDetailViewController : ViewController
 {
     /// <summary>
---
+++ src/Xenial.Framework.Win/SystemModule/Controllers/XenialHotReloadMainWindowController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+
+namespace Xenial.Framework.Win.SystemModule;
+
+/// <summary>
+/// For internal use only.
+/// </summary>
+public sealed class XenialHotReloadMainWindowController : WindowController
+{
+    /// <summary>
+    /// For internal use only.
+    /// </summary>
+    public XenialHotReloadMainWindowController()
+        => TargetWindowType = WindowType.Main;
+
+    /// <summary>
+    /// For internal use only.
+    /// </summary>
+    protected override void OnActivated()
+    {
+        base.OnActivated();
+        var controller = Frame.GetController<WindowTemplateController>();
+        if (controller is not null)
+        {
+            controller.CustomizeWindowStatusMessages -= Controller_CustomizeWindowStatusMessages;
+            controller.CustomizeWindowStatusMessages += Controller_CustomizeWindowStatusMessages;
+        }
+    }
+
+    private void Controller_CustomizeWindowStatusMessages(object? sender, CustomizeWindowStatusMessagesEventArgs e)
+    {
+        var hotreloadPipeName = System.Environment.GetEnvironmentVariable("DOTNET_HOTRELOAD_NAMEDPIPE_NAME");
+        if (!string.IsNullOrEmpty(hotreloadPipeName))
+        {
+            e.StatusMessages.Add("Hot-Reload enabled");
+        }
+    }
+
+    /// <summary>
+    /// For internal use only.
+    /// </summary>
+    protected override void OnDeactivated()
+    {
+        var controller = Frame.GetController<WindowTemplateController>();
+        if (controller is not null)
+        {
+            controller.CustomizeWindowStatusMessages -= Controller_CustomizeWindowStatusMessages;
+        }
+        base.OnDeactivated();
+    }
+}
