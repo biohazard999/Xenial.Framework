43b15dd619d04402ed98745cea613aff0d107e57
Manuel Grundner <m.grundner@delegate.at>
2021-12-15 12:08:54 +0100

    test: added basic properties

--- test/Xenial.Framework.Generators.Tests/LayoutBuilderGeneratorTests.DoesNotEmitDiagnosticIfPartial.DotNet4_6.verified.cs
+++ test/Xenial.Framework.Generators.Tests/LayoutBuilderGeneratorTests.DoesNotEmitDiagnosticIfPartial.DotNet4_6.verified.cs
@@ -4,6 +4,11 @@
 using System;
 using System.Runtime.CompilerServices;
 
+using Xenial.Framework.Layouts;
+using Xenial.Framework.Layouts.Items;
+using Xenial.Framework.Layouts.Items.Base;
+using Xenial.Framework.Layouts.Items.LeafNodes;
+
 namespace MyProject
 {
     [CompilerGenerated]
