988445fc4e4b86a9f3358772742a2fccf8528680
Manuel Grundner <m.grundner@delegate.at>
2021-12-24 09:50:00 +0100

    feat: add use i model model member view item

--- src/Xenial.Framework/Model/GeneratorUpdaters/ModelDetailViewLayoutNodesGeneratorUpdater.cs
+++ src/Xenial.Framework/Model/GeneratorUpdaters/ModelDetailViewLayoutNodesGeneratorUpdater.cs
@@ -49,16 +49,8 @@ public sealed partial class ModelDetailViewLayoutModelDetailViewItemsNodesGenera
 
                     if (viewItem is null)
                     {
-                        var newViewItem = viewItems.AddNode<IModelPropertyEditor>(layoutViewItemNode.Id);
-
+                        var newViewItem = viewItems.AddNode<IModelMemberViewItem>(layoutViewItemNode.Id);
                         newViewItem.PropertyName = layoutViewItemNode.ViewItemId;
-                        ////For whatever reason we need to reset the editor here
-                        ////may be removed if we switch this code to the IModelViewItems node builder
-                        if (newViewItem is IModelPropertyEditor oldPropertyEditor)
-                        {
-                            newViewItem.ClearValue(nameof(newViewItem.PropertyEditorType));
-                            //newViewItem.PropertyEditorType = typeof(DevExpress.ExpressApp.Editors.DetailPropertyEditor);
-                        }
                     }
                 }
 
