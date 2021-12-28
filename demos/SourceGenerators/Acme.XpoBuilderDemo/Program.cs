7b3e72438be0741d185f674dd50421793375f022
Manuel Grundner <m.grundner@delegate.at>
2021-12-28 15:34:16 +0100

    docs: added xpo source generators

--- demos/SourceGenerators/Acme.XpoBuilderDemo/Program.cs
+++ demos/SourceGenerators/Acme.XpoBuilderDemo/Program.cs
@@ -2,6 +2,8 @@
 
 using Acme.Module.BusinessObjects;
 
+using DevExpress.Xpo;
+
 using static Acme.Module.Helpers.DemoHelper;
 
 var session = CreateSession();
@@ -27,8 +29,33 @@ var person = new PersonBuilder()
 
 session.Save(person);
 
-
 WriteXPObjectToConsole(person);
+/*
+{
+  "FirstName": "John",
+  "LastName": "Doe",
+  "Manager": {
+    "FirstName": "Mary",
+    "LastName": "Major",
+    "Manager": null,
+    "Tasks": [],
+    "Oid": 1
+  },
+  "Tasks": [
+    {
+      "Description": "Use Xenial.Framework.Generators",
+      "Duration": "00:00:10",
+      "Oid": 2
+    },
+    {
+      "Description": "Install Xenial.Framework",
+      "Duration": "00:00:30",
+      "Oid": 1
+    }
+  ],
+  "Oid": 2
+}
+*/
 
 var manuel = new ManuelGrundnerPersonBuilder(session)
     .WithManager(person)
@@ -37,6 +64,75 @@ var manuel = new ManuelGrundnerPersonBuilder(session)
 session.Save(manuel);
 
 WriteXPObjectToConsole(manuel);
+/*
+{
+  "FirstName": "Manuel",
+  "LastName": "Grundner",
+  "Manager": {
+    "FirstName": "John",
+    "LastName": "Doe",
+    "Manager": {
+      "FirstName": "Mary",
+      "LastName": "Major",
+      "Manager": null,
+      "Tasks": [],
+      "Oid": 1
+    },
+    "Tasks": [
+      {
+        "Description": "Use Xenial.Framework.Generators",
+        "Duration": "00:00:10",
+        "Oid": 2
+      },
+      {
+        "Description": "Install Xenial.Framework",
+        "Duration": "00:00:30",
+        "Oid": 1
+      }
+    ],
+    "Oid": 2
+  },
+  "Tasks": [
+    {
+      "Description": "Implement SourceGenerators",
+      "Duration": "10675199.02:48:05.4775807",
+      "Oid": 3
+    },
+    {
+      "Description": "Test SourceGenerators",
+      "Duration": "10675199.02:48:05.4775807",
+      "Oid": 4
+    },
+    {
+      "Description": "Deploy SourceGenerators",
+      "Duration": "10675199.02:48:05.4775807",
+      "Oid": 5
+    }
+  ],
+  "Oid": 3
+}
+*/
 
 Console.ReadLine();
 
+internal class ManuelGrundnerPersonBuilder : PersonBuilder
+{
+    public ManuelGrundnerPersonBuilder(Session session)
+    {
+        WithSession(session);
+        WithFirstName("Manuel");
+        WithLastName("Grundner");
+        WithTasks(b => b
+            .WithDescription("Implement SourceGenerators")
+            .WithDuration(TimeSpan.MaxValue)
+        );
+        WithTasks(b => b
+            .WithDescription("Test SourceGenerators")
+            .WithDuration(TimeSpan.MaxValue)
+        );
+        WithTasks(b => b
+            .WithDescription("Deploy SourceGenerators")
+            .WithDuration(TimeSpan.MaxValue)
+        );
+    }
+}
