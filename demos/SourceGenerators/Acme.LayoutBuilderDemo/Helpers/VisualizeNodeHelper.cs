98cddf77302a29fe86b66b126a9ff8905888d52a
Manuel Grundner <m.grundner@delegate.at>
2022-01-01 15:58:37 +0100

    feat: demo of layout source generator

---
+++ demos/SourceGenerators/Acme.LayoutBuilderDemo/Helpers/VisualizeNodeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+using Acme.Module.Helpers.Xml;
+
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Utils;
+
+
+namespace Acme.Module.Helpers;
+
+#nullable enable
+
+internal static class VisualizeNodeHelper
+{
+    public static void PrintModelNode(this IModelNode modelNode)
+    {
+        var xml = UserDifferencesHelper.GetUserDifferences(modelNode)[""];
+        var prettyXml = new XmlFormatter().Format(xml);
+        Console.WriteLine(prettyXml);
+    }
+}
