70d175bd94c43864678bcfc140d93590ee61fc8c
Manuel Grundner <m.grundner@delegate.at>
2022-01-06 12:32:28 +0100

    feat: basic model mapper stub

---
+++ src/Xenial.Framework/Layouts/ViewOptionsMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xenial.Framework.Layouts;
+
+[Xenial.XenialModelOptionsMapper(typeof(DetailViewOptions))]
+internal partial class ViewOptionsMapper
+{
+}
