cce02f18b426234e04cb4efb1bbdcc50ff046618
Manuel Grundner <m.grundner@delegate.at>
2022-02-01 17:24:40 +0100

    refactor: view id attribute generator tests

---
+++ lic/Xenial.Framework.Generators/Attributes/XenialViewIdsAttributeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xenial.Framework.Generators.Base;
+using Xenial.Framework.MsBuild;
+
+namespace Xenial.Framework.Generators.Attributes;
+
+public record XenialViewIdsAttributeGenerator(bool AddSources = true) : XenialAttributeGenerator(AddSources)
+{
+    protected override string AttributeName => "XenialViewIdsAttribute";
+
+    protected override CurlyIndenter CreateAttribute(CurlyIndenter syntaxWriter, string visibility)
+    {
+        _ = syntaxWriter ?? throw new ArgumentNullException(nameof(syntaxWriter));
+
+        syntaxWriter.WriteLine($"using System;");
+        syntaxWriter.WriteLine($"using System.ComponentModel;");
+        syntaxWriter.WriteLine();
+
+        using (syntaxWriter.OpenBrace($"namespace {XenialNamespace}"))
+        {
+            syntaxWriter.WriteLine("[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]");
+
+            using (syntaxWriter.OpenBrace($"{visibility} sealed class {AttributeName} : Attribute"))
+            {
+                syntaxWriter.WriteLine($"{visibility} {AttributeName}() {{ }}");
+            }
+        }
+
+        return syntaxWriter;
+    }
+}
