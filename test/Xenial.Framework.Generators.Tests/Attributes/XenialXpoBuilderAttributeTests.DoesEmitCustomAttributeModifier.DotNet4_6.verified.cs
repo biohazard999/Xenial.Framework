ef4a8e4578cfcae3174fa290e2fd27f3814262ec
Manuel Grundner <m.grundner@delegate.at>
2022-02-01 18:03:57 +0100

    feat: moved xpo builder attribute

---
+++ test/Xenial.Framework.Generators.Tests/Attributes/XenialXpoBuilderAttributeTests.DoesEmitCustomAttributeModifier.DotNet4_6.verified.cs
@@ -0,0 +1,12 @@
+//HintName: XenialXpoBuilderAttribute.g.cs
+using System;
+using System.ComponentModel;
+
+namespace Xenial
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class XenialXpoBuilderAttribute : Attribute
+    {
+        public XenialXpoBuilderAttribute() { }
+    }
+}
---
+++ test/Xenial.Framework.Generators.Tests/Attributes/XenialXpoBuilderAttributeTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+using Xenial.Framework.Generators.Attributes;
+
+namespace Xenial.Framework.Generators.Tests.Attributes;
+
+public class XenialXpoBuilderAttributeTests : AttributeGeneratorBaseTests<XenialXpoBuilderAttributeGenerator>
+{
+    protected override XenialXpoBuilderAttributeGenerator CreateTargetGenerator()
+        => new XenialXpoBuilderAttributeGenerator(false);
+}
