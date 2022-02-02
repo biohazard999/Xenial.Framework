3521df015bdf04effe08b0f6442f0dbc8a2211ba
Manuel Grundner <m.grundner@delegate.at>
2022-02-02 14:42:09 +0100

    fix: updated packages to support netcoreapp3.1

---
+++ test/Xenial.Framework.Generators.Tests/Attributes/XenialViewIdsAttributeTests.DoesEmitIfSpecificMsBuildProperty_emitProperty=True.Core3_1.verified.cs
@@ -0,0 +1,12 @@
+//HintName: XenialViewIdsAttribute.g.cs
+using System;
+using System.ComponentModel;
+
+namespace Xenial
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    internal sealed class XenialViewIdsAttribute : Attribute
+    {
+        internal XenialViewIdsAttribute() { }
+    }
+}
