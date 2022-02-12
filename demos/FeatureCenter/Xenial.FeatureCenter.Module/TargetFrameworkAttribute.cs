be4b97b47b4c479926eeb2e6591f9a5f9d907098
Manuel Grundner <m.grundner@delegate.at>
2022-02-12 10:06:03 +0100

    feat: added tfm in statusbar for winforms

---
+++ demos/FeatureCenter/Xenial.FeatureCenter.Module/TargetFrameworkAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xenial.FeatureCenter.Module;
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
+public sealed class TargetFrameworkAttribute : Attribute
+{
+    public string TargetFramework { get; }
+
+    public TargetFrameworkAttribute(string targetFramework)
+        => TargetFramework = targetFramework;
+}
