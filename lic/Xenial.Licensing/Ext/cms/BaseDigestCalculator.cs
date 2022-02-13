5232e7468d9e8532b627b9f6f5122a46dc28e8f1
Manuel Grundner <m.grundner@delegate.at>
2022-02-13 10:48:51 +0100

    feat: copied code to lic

---
+++ lic/Xenial.Licensing/Ext/cms/BaseDigestCalculator.cs
@@ -0,0 +1,24 @@
+
+using Org.BouncyCastle.Utilities;
+
+using System;
+
+namespace Org.BouncyCastle.Cms
+{
+    internal class BaseDigestCalculator
+        : IDigestCalculator
+    {
+        private readonly byte[] digest;
+
+        internal BaseDigestCalculator(
+            byte[] digest)
+        {
+            this.digest = digest;
+        }
+
+        public byte[] GetDigest()
+        {
+            return Arrays.Clone(digest);
+        }
+    }
+}
