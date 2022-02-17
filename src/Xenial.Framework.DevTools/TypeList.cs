76f3da73640aa9dad8a1c2774e4a81c8976f5b10
Manuel Grundner <m.grundner@delegate.at>
2022-02-17 09:54:36 +0100

    feat: try to use exported types generator

---
+++ src/Xenial.Framework.DevTools/TypeList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevExpress.ExpressApp;
+
+[Xenial.CollectExportedTypes]
+public static partial class TypeList
+{
+}
