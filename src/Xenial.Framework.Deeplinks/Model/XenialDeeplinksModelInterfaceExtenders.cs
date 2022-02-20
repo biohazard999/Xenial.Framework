d6acb7db00860bbd68437966509d719b13374565
Manuel Grundner <m.grundner@delegate.at>
2022-02-20 18:54:22 +0100

    feat basic usage interfaces

--- src/Xenial.Framework.Deeplinks/Model/XenialDeeplinksModelInterfaceExtenders.cs
+++ src/Xenial.Framework.Deeplinks/Model/XenialDeeplinksModelInterfaceExtenders.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using DevExpress.ExpressApp;
@@ -8,8 +9,17 @@ using Xenial.Framework.Deeplinks.Model;
 
 namespace DevExpress.ExpressApp.Model;
 
-internal static class XenialDeeplinksModelInterfaceExtenders
+/// <summary>
+/// 
+/// </summary>
+public static class XenialDeeplinksModelInterfaceExtenders
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="extenders"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
     public static ModelInterfaceExtenders UseXenialDeeplinks(this ModelInterfaceExtenders extenders)
     {
         _ = extenders ?? throw new ArgumentNullException(nameof(extenders));
@@ -17,5 +27,23 @@ internal static class XenialDeeplinksModelInterfaceExtenders
         extenders.Add<IModelOptions, IModelDeeplinkProtocols>();
 
         return extenders;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="regularTypes"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    //TODO: move to code generator and correct namespace
+    public static IEnumerable<Type> UseXenialDeeplinksRegularTypes(this IEnumerable<Type> regularTypes)
+    {
+        _ = regularTypes ?? throw new ArgumentNullException(nameof(regularTypes));
+        return regularTypes.Concat(new[]
+       
+            typeof(IModelDeeplinkProtocols),
+            typeof(IModelDeeplinkProtocol),
+            typeof(ModelDeeplinkProtocolLogic),
+        });
     }
 }
