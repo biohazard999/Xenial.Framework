251cfdb4c4c911c9e9c18b7f96ed6faea783046e
Manuel Grundner <m.grundner@delegate.at>
2022-02-21 18:41:40 +0100

    feat: added Xaf deep link

---
+++ src/Xenial.Framework.Deeplinks/XafDeeplinkSingleInstance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using DevExpress.ExpressApp;
+
+namespace Xenial.Framework.Deeplinks;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class XafDeeplinkSingleInstance : DeeplinkSingleInstance
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="application"></param>
+    public XafDeeplinkSingleInstance(XafApplication application) : base(application.ApplicationName)
+    {
+    }
+}
