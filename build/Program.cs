45019f2bd3b5643865ef3d374aeb46716d3696c3
Manuel Grundner <m.grundner@delegate.at>
2022-02-22 14:24:53 +0100

    build: added debugable packages

--- build/Program.cs
+++ build/Program.cs
@@ -306,7 +306,7 @@ namespace Xenial.Build
                 () => RunAsync("dotnet", $"pack {sln} --no-restore --no-build -c {Configuration} {logOptions("pack.nuget")} {GetProperties()}")
             );
 
-            Target("pack:debug", DependsOn("lic"),
+            Target("pack:debug", DependsOn("lic", "build:debug"),
                 () => RunAsync("dotnet", $"pack {sln} --no-restore --no-build -c {ConfigurationDebug} {logOptions("pack.nuget.debug")} {GetProperties()}")
             );
 
