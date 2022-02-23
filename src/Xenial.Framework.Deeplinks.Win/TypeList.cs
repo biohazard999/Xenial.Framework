3d17c8739387c07cfd4f85ca8f39e4a764a69a9b
Manuel Grundner <m.grundner@delegate.at>
2022-02-23 14:52:19 +0100

    feat: moved to dx namespace

--- src/Xenial.Framework.Deeplinks.Win/TypeList.cs
+++ src/Xenial.Framework.Deeplinks.Win/TypeList.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Xenial.Framework.Deeplinks.Win;
+namespace DevExpress.ExpressApp;
 
 /// <summary>
 /// 
