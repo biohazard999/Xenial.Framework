0165a93fa803640a6117d57dbcf50980257c333c
Manuel Grundner <m.grundner@delegate.at>
2022-02-24 10:05:27 +0100

    feat: added winui

--- src/Xenial.Framework.Deeplinks.Win/Controllers/BringToFrontDeeplinkMainWindowController.cs
+++ src/Xenial.Framework.Deeplinks.Win/Controllers/BringToFrontDeeplinkMainWindowController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Win;
+
+using System;
+
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Xenial.Framework.Deeplinks.Win;
 
