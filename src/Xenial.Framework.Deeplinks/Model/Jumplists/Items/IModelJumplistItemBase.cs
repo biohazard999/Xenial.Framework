e2cc4cd355fa97ba983ede46c6a6135838cdfbeb
Manuel Grundner <m.grundner@delegate.at>
2022-03-01 11:23:36 +0100

    feat: getter & setter

--- src/Xenial.Framework.Deeplinks/Model/Jumplists/Items/IModelJumplistItemBase.cs
+++ src/Xenial.Framework.Deeplinks/Model/Jumplists/Items/IModelJumplistItemBase.cs
@@ -37,14 +37,14 @@ public interface IModelJumplistItemBase : IModelJumplistItem
     /// 
     /// </summary>
     [ModelBrowsable(typeof(ProtocolArgumentsVisibilityCalculator))]
-    string Arguments { get; }
+    string Arguments { get; set; }
 
     /// <summary>
     /// 
     /// </summary>
     [ModelBrowsable(typeof(ProtocolLaunchUriVisibilityCalculator))]
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:URI-like properties should not be strings")]
-    string LaunchUri { get; }
+    string LaunchUri { get; set; }
 }
 
 /// <summary>
