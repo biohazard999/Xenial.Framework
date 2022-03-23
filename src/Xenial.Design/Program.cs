0b2df6e84ebc5defb4d5a9983752eeb072f412f4
Manuel Grundner <m.grundner@delegate.at>
2022-03-23 13:12:09 +0100

    feat: added stream rcp json

--- src/Xenial.Design/Program.cs
+++ src/Xenial.Design/Program.cs
@@ -1,15 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using Spectre.Console;
+
+using StreamJsonRpc;
+
+using System;
+using System.IO;
+using System.IO.Pipes;
+
 using System.Linq;
-using System.Text;
+
 using System.Threading.Tasks;
 
-namespace Xenial.Design;
+using Xenial.Design;
 
-public static class Program
+try
 {
-    public static void Main(string[] args)
-    {
+    var connectionId = args[0];
 
+    await NamedPipeServerAsync(connectionId);
+
+    return 0;
+}
+catch (Exception ex)
+{
+    AnsiConsole.WriteException(ex);
+    return 1;
+}
+
+
+static async Task NamedPipeServerAsync(string connectionId)
+{
+    var clientId = 0;
+    while (true)
+    {
+        await Console.Error.WriteLineAsync("Waiting for client to make a connection...");
+        using (var stream = new NamedPipeServerStream(connectionId, PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+        {
+            await stream.WaitForConnectionAsync();
+            _ = RespondToRpcRequestsAsync(stream, ++clientId);
+        }
     }
 }
+
+static async Task RespondToRpcRequestsAsync(Stream stream, int clientId)
+{
+    AnsiConsole.WriteLine($"Connection request #{clientId} received. Spinning off an async Task to cater to requests.");
+    var jsonRpc = JsonRpc.Attach(stream, new Server());
+    AnsiConsole.WriteLine($"JSON-RPC listener attached to #{clientId}. Waiting for requests...");
+    await jsonRpc.Completion;
+    AnsiConsole.WriteLine($"Connection #{clientId} terminated.");
+}
+
