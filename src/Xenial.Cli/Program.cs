795587c216460fc55d5b398d63e7e547327a3ad1
Manuel Grundner <m.grundner@delegate.at>
2022-03-26 10:27:26 +0100

    feat: launch debugger

--- src/Xenial.Cli/Program.cs
+++ src/Xenial.Cli/Program.cs
@@ -17,10 +17,20 @@ Console.InputEncoding = Encoding.UTF8;
 var logVerbosityOption = new Option<LogLevel?>(new[] { "-v", "--verbosity" });
 var logFileOption = new Option<bool?>(new[] { "--log-file" });
 var workingDirectoryOption = new Option<string?>(new[] { "-w", "--working-directory" });
+var debugOption = new Option<bool?>(new[] { "--debug" });
 
 var logLvl = Xenial.Cli.Utils.CommandLineHelper.GetGlobalOptions(args, logVerbosityOption) ?? LogLevel.Error;
 var logFile = Xenial.Cli.Utils.CommandLineHelper.GetGlobalOptions(args, logFileOption) ?? false;
 var workingDirectory = Xenial.Cli.Utils.CommandLineHelper.GetGlobalOptions(args, workingDirectoryOption);
+var debug = Xenial.Cli.Utils.CommandLineHelper.GetGlobalOptions(args, debugOption);
+
+if (debug.HasValue && debug.Value)
+{
+    if (!Debugger.IsAttached)
+    {
+        Debugger.Launch();
+    }
+}
 
 var services = new ServiceCollection();
 
